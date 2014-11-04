8bf4d9570ada3c2a31b1a2a522a32c68eae1d50e
Jack Thakar <jathak@gmail.com>
2014-11-03 21:20:46 -0800

    everything should work now

--- bin/targets.dart
+++ bin/targets.dart
@@ -129,12 +129,10 @@ gitLoad(String url, String id, bool isTeacher, [String newOwner]){
                         Process.start("git",['clone',url]).then((prc) {
                             prc.exitCode.then((ec){
                                 new Directory("targets-$id").renameSync(id);
-                                String baseName = Platform.script.toFilePath();
-                                baseName = baseName.substring(0, baseName.length-12);
-                                String testerName = baseName + "tester.dart";
-                                String helperName = baseName + "helpers.dart";
-                                new File(testerName).copySync("$id/targets/tester.dart");
-                                new File(helperName).copySync("$id/targets/helpers.dart");
+                                File testerFile = new File("$id/targets/tester.dart");
+                                File helperFile = new File("$id/targets/helpers.dart");
+                                testerFile.writeAsStringSync(tester_dart);
+                                helperFile.writeAsStringSync(helpers_dart);
                                 new Directory("$id/.git").deleteSync(recursive: true);
                                 if(!isTeacher){
                                     File tests = new File("$id/targets/tests.dart");
@@ -237,6 +235,144 @@ Function print = (String str, [String type=PLAIN]){
         stdout.writeln("\u001b[0;36m"+str+"\u001b[0;0m");
     }
 };
+
+
+/// Master helpers.dart and tester.dart (since pub global breaks the old way)
+final String helpers_dart =
+r"""// targets uses this file to test your code
+// Changing it will not improve your grade
+// It will only make it harder for you to run tests
+
+abstract class Target{
+    Function test;
+    String name;
+    String error;
+}
+
+/// This creates an unscored target
+/// [test] should return a bool
+class TestTarget extends Target{
+    Function test = ()=>false;
+    String name;
+
+    TestTarget(this.name, [Function test()]);
+}
+
+/// This creates a scored target
+/// [test] can return a number (equal to points earned)
+/// or a bool (true is full credit, false is no credit)
+class ScoredTarget extends Target{
+    Function test = ()=>0;
+    String name;
+    num points;
+
+    ScoredTarget(this.name, this.points);
+}""";
+
+final String tester_dart = 
+r"""// targets uses this file to test your code
+// Changing it will not improve your grade
+// It will only make it harder for you to run tests
+
+import 'dart:io';
+import 'tests.dart' as Tests;
+import 'helpers.dart';
+
+main(List<String> args){
+    if(args.length==0)runTests();
+    else if(args[0]=="submit"){
+        print(Tests.owner+":"+Tests.id);
+        for(String file in Tests.files){
+            print(file);
+        }
+    }
+}
+
+void runTests(){
+    try{
+        print(Tests.name, BLUE);
+        print("");
+    }catch(e){
+
+    }
+    List<Target> targets = Tests.getTargets();
+    num score = 0;
+    num maxPoints = 0;
+    bool allPassed = true;
+    for(Target t in targets){
+        if(t is ScoredTarget){
+            maxPoints += t.points;
+            var s = 0;
+            try{
+                s = t.test();
+                if(s==null)s=0;
+            }catch(e){
+                String error = e.toString().replaceAll("\n"," ");
+                print("Test ${t.name} failed with error: $error", RED);
+            }
+            if(s is bool){
+                if(s){
+                    s = t.points;
+                    score += t.points;
+                }else s = 0;
+            }else if(s != null) score += s;
+            String extra = "";
+            if(t.error!=null){
+                extra = "- ${t.error}";
+            }
+            print("${t.name}: $s/${t.points} $extra");
+        }else if(t is TestTarget){
+            bool result = false;
+            try{
+                result = t.test();
+                if(result==null)result=false;
+            }catch(e){
+                String error = e.toString().replaceAll("\n"," ");
+                print("Test ${t.name} failed with error: $error", RED);
+            }
+            String extra = "";
+            if(t.error!=null){
+                extra = "- ${t.error}";
+            }
+            if(result){
+                print("${t.name}: Passed $extra");
+            }else{
+                print("${t.name}: Failed $extra");
+                allPassed = false;
+            }
+        }
+    }
+    if(maxPoints>0){
+        if(score>=maxPoints) print("Total Score: $score/$maxPoints", GREEN);
+        else print("Total Score: $score/$maxPoints", RED);
+        if(!allPassed) print("Some Additional Tests Failed", RED);
+    }else{
+        if(allPassed) print("All Tests Passed!", GREEN);
+        else print("Some Tests Failed", RED);
+    }
+}
+
+const String PLAIN = "plain";
+const String GREEN = "green";
+const String RED = "red";
+const String BLUE = "blue";
+
+Function yesprint = (String str, [String type=PLAIN]){
+    if(type==PLAIN||Platform.isWindows){
+        stdout.writeln(str);
+    }else if(type==RED){
+        stdout.writeln("\u001b[0;31m"+str+"\u001b[0;0m");
+    }else if(type==GREEN){
+        stdout.writeln("\u001b[0;32m"+str+"\u001b[0;0m");
+    }else if(type==BLUE){
+        stdout.writeln("\u001b[0;36m"+str+"\u001b[0;0m");
+    }
+};
+
+Function print = yesprint;
+
+Function noprint = (String str){};
+""";
 
 class Base64 {
   static const List<String> _encodingTable = const [
