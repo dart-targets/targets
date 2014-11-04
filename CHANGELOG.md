2d52eb24d589dfbf0e2a61ae698ef1982ec4b0e8
Jack Thakar <jathak@gmail.com>
2014-11-04 11:25:47 -0800

    added IOTarget

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+#### 0.2.3
+  * Added `IOTarget`, which allows testing of other languages. For an example in Java, download the `java-example` assignment with `targets get` or `targets init`
+
+#### 0.2.2
+  * Fixed issues; should work on OSX, Windows, and Linux now
+
+#### 0.2.1
+  * First release
--- bin/targets.dart
+++ bin/targets.dart
@@ -3,7 +3,7 @@
 import 'dart:io';
 import 'dart:convert';
 
-const String VERSION = "0.2.1";
+const String VERSION = "0.2.3";
 
 void main(var args){
     if(Platform.isWindows){
@@ -241,6 +241,8 @@ final String helpers_dart =
 r"""// targets uses this file to test your code
 // Changing it will not improve your grade
 // It will only make it harder for you to run tests
+import 'dart:io';
+import 'dart:convert';
 
 abstract class Target{
     Function test;
@@ -266,7 +268,106 @@ class ScoredTarget extends Target{
     num points;
 
     ScoredTarget(this.name, this.points);
+}
+
+/// This creates a TestTarget that passes standard input
+/// to a program that is run and checks if the output
+/// matches what's provided
+/// This involves several hacks, so it may break in the future
+/// Also, using this will require you to download code and
+/// batch test, as dart:io isn't available in the browser
+class IOTarget extends TestTarget{
+
+    /// command - Command that input is passed to
+    /// input - Input (string or file) to pass to command
+    /// output - Output (string or file) to match against command output
+    /// preCommand - Command or list of commands to run
+    ///             before passing in input
+    /// postCommand - Command or list commands to run
+    ///             after passing in input
+    IOTarget(String name, String command, var input, var output, 
+                        [var preCommand, var postCommand]):super(name){
+        test = (){
+            if(input is File){
+                input = input.readAsStringSync();
+            }
+            if(output is File){
+                output = output.readAsStringSync();
+            }
+            if(preCommand!=null){
+                if(preCommand is String) runCommand(preCommand);
+                else{
+                    for(String str in preCommand) runCommand(str);
+                }
+            }
+            var parts = command.split(" ");
+            var exe = parts.removeAt(0);
+            String pstr = "";
+            for(String str in parts){
+                pstr+="'$str',";
+            }
+            new File(".tempscript.dart").writeAsStringSync('''"""+io_script+r"""''');
+            String out = Process.runSync('dart',['.tempscript.dart']).stdout;
+            if(Platform.isWindows){
+                out = out.replaceAll("\r\n","\n");
+            }
+            if(postCommand!=null){
+                if(postCommand is String) runCommand(postCommand);
+                else{
+                    for(String str in postCommand) runCommand(str);
+                }
+            }
+            if(Platform.isWindows) Process.runSync('del',['.tempscript.dart'],runInShell:true);
+            else Process.runSync('rm',['.tempscript.dart']);
+            bool result = output==out||output+"\n"==out;
+            if(!result){
+                this.error = "Expected $output, got $out";
+            }
+            return result;
+        };
+    }
+
+    static IOTarget makeJava(String name, String mainClass, String input, 
+                        String output, [List<String> otherClasses]){
+        List<String> pre = ["javac $mainClass.java"];
+        List<String> post = ["rm $mainClass.class"];
+        String command = "java $mainClass";
+        if(otherClasses != null){
+            for(String str in otherClasses){
+                pre.add("javac $str.java");
+                post.add("rm $str.class");
+            }
+        }
+        return new IOTarget(name, command, input, output, pre, post);
+    }
+
+    runCommand(String command){
+        var parts = command.split(" ");
+        var exe = parts.removeAt(0);
+        if(exe=="rm"&&Platform.isWindows){
+            exe = "del";
+        }
+        Process.runSync(exe, parts, runInShell:true);
+    }
 }""";
+
+/// Used by IOTarget in helpers.dart
+final String io_script = r'''
+import 'dart:io';
+import 'dart:convert';
+main(){
+    Process.start("$exe",[$pstr]).then((process) {
+        process.stdout.transform(UTF8.decoder)
+                .transform(new LineSplitter()).listen((data){
+            stdout.writeln(data);
+        });
+        for(String str in """$input""".split("\\n")){
+            process.stdin.writeln(str);
+        }
+        process.stdin.close();
+    });
+}
+''';
 
 final String tester_dart = 
 r"""// targets uses this file to test your code
