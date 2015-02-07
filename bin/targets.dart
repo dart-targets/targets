052c15554fae49f0f46278599bf7d212645eb747
Jack Thakar <jathak@gmail.com>
2015-02-07 00:21:21 -0800

    version change

--- bin/targets.dart
+++ bin/targets.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 
-const String VERSION = "0.5.0-testing";
+const String VERSION = "0.5.0";
 
 void main(var args){
     if(Platform.isWindows){
